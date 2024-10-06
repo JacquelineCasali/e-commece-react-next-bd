e90ded33bfc06c2561fe650a3126dff546d220ea
Jacqueline <jcasalia@yahoo.com.br>
2024-10-06 09:32:29 -0300

    banco de dados

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProdutoModule } from './produto/produto.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ProdutoModule],
+  imports: [ProdutoModule, DbModule],
 
   controllers: [AppController],
   providers: [],
--- backend/src/produto/produto.controller.ts
+++ backend/src/produto/produto.controller.ts
@@ -1,21 +1,30 @@
 /* eslint-disable prettier/prettier */
 
-import { Controller, Get, Param } from '@nestjs/common';
-import { Produto, produtos } from 'src/core';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Produto } from 'src/core';
+import { ProdutoPrisma } from './produto.prisma';
 
 @Controller('produtos')
 export class ProdutoController {
+constructor(readonly repo:ProdutoPrisma){}
+
+
+@Post()
+    salvarProdutos(@Body() produto : Produto):Promise<void>{
+        return this.repo.salvar(produto);
+    }
+
 @Get()
-    obterProdutos():Produto[]{
-        return produtos.map((produto)=>({
-            ...produto,
-            especificacoes:{destaque:produto.especificacoes.destaque},
-        }));
+    obterProdutos():Promise<Produto[]>{
+        return this.repo.obter();
     }
     @Get(':id')
     async obterProdutoPorId(@Param('id') id: string): Promise<Produto | null> {
-      const produto = produtos.find((produto) => produto.id === +id);
-      return produto ?? null;
+      return this.repo.obterPorId(Number(id))
     }
+@Delete(':id')
+async DeletarProduto(@Param('id') id: string): Promise<void> {
+    return await this.repo.excluirProduto(+id)
+  }
 
 }
